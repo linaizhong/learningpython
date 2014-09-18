95dcbb818199cf4395f2246c500ca251c1824223
brunogabriel <bruno.gabriel@phocus.com.br>
2014-09-18 16:44:47 -0300

    Added the minidom insertion

--- features/write_read/xml/xml_work.py
+++ features/write_read/xml/xml_work.py
@@ -6,6 +6,7 @@ https://docs.python.org/2/library/xml.html
 """
 
 import xml.etree.ElementTree as ET
+import xml.dom.minidom
 
 """Etree method"""
 def read_data_etree():
@@ -37,3 +38,35 @@ def write_data_etree():
 	rank.text = str(12)
 	# escreve o resultado do XML
 	ET.dump(root)
+
+
+"""Usa o minidom para gerar um conteudo com ideias semelhantes ao countries.xml"""
+def write_data_minidom():
+	# Gera um documento
+	document = xml.dom.minidom.Document()
+
+	root = document.createElement('data')
+
+	country = document.createElement('country')
+
+	rank = document.createElement('rank')
+	rank.appendChild(document.createTextNode('your rank here'))
+	neighbor = document.createElement('neighbor')
+	neighbor.setAttribute('name', 'Austria')
+
+	
+	document.appendChild(root)
+
+	root.appendChild(country)
+	root.appendChild(rank)
+
+	country.appendChild(rank)
+	country.appendChild(neighbor)
+	
+
+	# mostra o XML
+	print document.toprettyxml()
+
+
+
+
